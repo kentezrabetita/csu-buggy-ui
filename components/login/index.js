40489036f3516b156f7a28f8befd7f1aa9bef4d7
kentezrabetita <kentezrabetita@gmail.com>
2022-06-13 17:29:17 +0800

    add login component and link to dashboard

--- components/login/index.js
+++ components/login/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Login() {
   return (
-    <div className='h-screen flex items-center justify-center text-center text-red-500'>
-      Login Component
+    <div className='container mx-auto'>
+      <div className='flex items-center justify-center h-screen'>
+        <form className='w-full px-6 max-w-sm'>
+          <div className='flex flex-col space-y-6 bg-purple-500 w-full p-6 rounded-lg'>
+            <div>
+              <h1 className='text-white text-center text-3xl font-bold'>
+                ADMIN LOGIN
+              </h1>
+            </div>
+            <div>
+              <input
+                type='text'
+                name='username'
+                placeholder='USERNAME'
+                className='p-4 border rounded-lg w-full'
+              />
+            </div>
+            <div>
+              <input
+                type='password'
+                name='password'
+                placeholder='PASSWORD'
+                className='p-4 border rounded-lg w-full'
+              />
+            </div>
+            <div>
+              <Link href='/dashboard'>
+                <a>
+                  <button className='p-4 bg-white rounded-lg w-full'>
+                    LOGIN
+                  </button>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
