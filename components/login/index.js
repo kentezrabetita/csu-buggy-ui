f62d08a04c64143b9f55056ccc4aa41fb1793ea5
kentezrabetita <kentezrabetita@gmail.com>
2022-06-13 14:57:04 +0800

    setup pages, components, and tailwind

---
+++ components/login/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className='h-screen flex items-center justify-center text-center text-red-500'>
+      Login Component
+    </div>
+  );
+}
