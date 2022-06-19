fb5c38543a04b3cbfe2a726aa095f3be8c4c18fd
kentezrabetita <kentezrabetita@gmail.com>
2022-06-20 04:24:55 +0800

    move dashboard component to items route

---
+++ pages/items/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from '../../components/dashboard';
+
+export default function Items() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
