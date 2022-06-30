f326de831e09ee3226af3655f8634385fc367c58
kentezrabetita <kentezrabetita@gmail.com>
2022-06-30 08:58:34 +0800

    add modal background blur

--- components/modal/blur.js
+++ components/modal/blur.js
@@ -4,7 +4,7 @@ export default function Blur({ onClose }) {
   return (
     <div
       onClick={onClose}
-      className='fixed inset-0 w-full h-full overflow-y-auto bg-gray-600 bg-opacity-50'
+      className='fixed inset-0 w-full h-full overflow-y-auto bg-gray-600 bg-opacity-50 backdrop-blur-sm'
     ></div>
   );
 }
